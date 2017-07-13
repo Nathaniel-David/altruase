959b248f48b285488688fa9ae9aea413cd5de0f0
Nathaniel-David <me@nathaniel-david.tech>
2017-07-13 12:49:03 -0500

    cypher key

--- altruase/config/app.php
+++ altruase/config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('z3WhT2UfBaRgG856R7aVXZXPFVCanzTX/oCnv+ZZq+M='),
 
     'cipher' => 'AES-256-CBC',
 
